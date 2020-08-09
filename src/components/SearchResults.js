01a7bdfff64244a438d3337fc159fbd73bbe7d0c
Dorje <ku.dorjee@gmail.com>
2020-08-09 22:00:36 +0545

    Stat component

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,18 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Row, Col, InputGroup } from 'react-bootstrap';
 import { ResultsWrapper, RepoCard, RepoName, ShortDescriptions, StatWrapper } from './styled-component.styled';
 import { GoRepoForked } from 'react-icons/go';
 import { AiFillEye, AiFillStar } from 'react-icons/ai';
 
-//repo name 
-//author 
-//number of stars, forks and watchers
-//
-
-
 export default function SearchResults(props){
+
+  //Substring of the full descriptions
 	const shortDescriptions = (descriptions) => {
-		if(descriptions ===null)
+		if(descriptions === null)
 		 return descriptions;
 
 		return descriptions.substring(0, 100)
@@ -22,13 +18,12 @@ export default function SearchResults(props){
 		<ResultsWrapper>
 			<Row>
 				{ props.repos.map( repo => {
-					debugger
 					return(
-						<Col md={3} >
+						<Col md={4} >
 							<RepoCard className="card">
 								<RepoName>{repo.name}</RepoName>
-								<ShortDescriptions>{shortDescriptions(repo.description)}</ShortDescriptions>
-								<Stats stars={10} forks={20} watchers={30}/>
+								<ShortDescriptions>{shortDescriptions(repo.description)}</ShortDescriptions>        
+								<Stats stars={repo.stargazers_count} forks={repo.forks} watchers={repo.subscribers_url}/>
 							</RepoCard>
 						</Col>
 					)
@@ -39,20 +34,28 @@ export default function SearchResults(props){
 }
 
 function Stats(props){
+  // const [ subscribersCount, setSubscribersCount ] = useState(0)
+
+  // useEffect( async () => {
+  //   const count = await axios(`${props.watchers}`)
+  //   setSubscribersCount(count.data.length())
+  // })
+
+
   return(
     <StatWrapper>
-      <InputGroup className="input-sm">
+      <InputGroup size="sm">
         <InputGroup.Text>  
-          <AiFillEye></AiFillEye> 10
+          <AiFillEye></AiFillEye> 
         </InputGroup.Text>
         <InputGroup.Append>
           <InputGroup.Text>
-          <GoRepoForked></GoRepoForked> 20
+            <GoRepoForked></GoRepoForked> {props.forks}
           </InputGroup.Text>
         </InputGroup.Append>
         <InputGroup.Append>
           <InputGroup.Text>
-          <AiFillStar></AiFillStar> 30
+            <AiFillStar></AiFillStar> {props.stars}
           </InputGroup.Text>
         </InputGroup.Append>
       </InputGroup>
