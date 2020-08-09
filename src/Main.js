714a7c559916daed68a2897394b6ddf81dcb5f25
Dorje <ku.dorjee@gmail.com>
2020-08-09 13:21:52 +0545

    search functionality

--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,27 @@
-import React from 'react';
-import Search from './components';
+import React, { useState, useEffect } from 'react';
+import { Search, SearchResults } from './components';
+import { searchRepo } from './api.js';
 
 //Main page
 function Main() {
+  const [ results, setResults ] = useState([])
+  const [ totalCount, setTotalCount ] = useState(0)
+
+  const onSearch = (keyword) =>{
+    searchRepo(keyword, '', '')
+      .then( res => {
+        setResults(res.data.items)
+        setTotalCount(res.data.total_count)
+      })
+      .catch( err => {
+        alert(err.message)
+      })
+  }
+
   return (
     <div>
-      <Search />
+      <Search onSearch={onSearch}/>
+      <SearchResults repos={results} totalCount={totalCount}/>
     </div>
   );
 }
