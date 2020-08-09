dc113cbce1ee710b41f8dcdf33edb0f0bc129f60
Dorje <ku.dorjee@gmail.com>
2020-08-09 22:01:03 +0545

    export components

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
-import styled from 'styled-components';
-
 export { default as Search } from './Search';
 export { default as SearchResults } from './SearchResults';
+export { default as Menu } from './Menu';
