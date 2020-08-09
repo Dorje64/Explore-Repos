d42102bb8c1240be0578b1a435d930a525a2045e
Dorje <ku.dorjee@gmail.com>
2020-08-09 13:24:40 +0545

    add styled-components for UI

--- src/components/styled-component.styled.js
+++ src/components/styled-component.styled.js
@@ -4,5 +4,34 @@ export const SearchWrapper = styled.div`
   width: 100%;
   display: flex;
   justify-content: center;
-  padding: 200px 500px 0 500px; 
+  padding: 200px 500px 0 500px;
+`
+
+export const ResultsWrapper = styled.div`
+  width: 100%;
+  padding: 50px 100px 50px 100px;
+`
+
+export const RepoCard = styled.div`
+  width: 100%;
+  padding: 10px;
+  height: 180px;
+  font-size: 13px;
+  margin-bottom: 10px;
+`
+
+export const ShortDescriptions = styled.span`
+  color: grey;
+`
+
+export const RepoName = styled.h4`
+  color: blue;
+`
+
+export const StatWrapper = styled.div`
+  height: 10x;
+  bottom: 5px;
+  right: 5px;
+  position: absolute;
+
 `
