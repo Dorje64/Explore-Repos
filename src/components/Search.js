754c6fcba63aa881f4fa47d09f03d6b37113f208
Dorje <ku.dorjee@gmail.com>
2020-08-09 13:22:41 +0545

    impliment react events on search inputs/form

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,22 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { InputGroup, FormControl } from 'react-bootstrap'
 import { SearchWrapper } from './styled-component.styled.js';
 import { FiSearch } from 'react-icons/fi';
 import './style.css';
 
 //search input component
-export default function Search() {
+export default function Search(props) {
+	const [ keyword, setKeyword ] = useState('')
+
+	const handleChange = event => {
+		setKeyword(event.target.value)
+	}
+
+	const onEnter = event => {
+		// Number 13 is the "Enter" key on the keyboard
+		if (event.keyCode === 13) {
+			// Cancel the default action, if needed
+			event.preventDefault();
+			keyword.length > 0 && props.onSearch(keyword)
+		}
+	}
+
+	const clickSearch = _ => {
+		keyword.length > 0 && props.onSearch(keyword)
+	}
+
 	return (
 		<SearchWrapper>
 			<InputGroup className="search-input">
 				<FormControl
-					placeholder="Search Repositor"
+					placeholder="Search Repositories"
+					onChange={ handleChange }
+					onKeyDown={ onEnter }
 				/>
 				<InputGroup.Append>
 					<InputGroup.Text>
-						<FiSearch />
+						<FiSearch  onClick={clickSearch} style={{cursor: 'pointer'}}/>
 					</InputGroup.Text>
 				</InputGroup.Append>
 			</InputGroup>
 		</SearchWrapper>)
-}	
+}
