344199289f9dfacabcb13f0b15df2191994315cb
Dorje <ku.dorjee@gmail.com>
2020-08-09 13:23:11 +0545

    export components form single point

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 import styled from 'styled-components';
 
-export { default } from './Search';
+export { default as Search } from './Search';
+export { default as SearchResults } from './SearchResults';
