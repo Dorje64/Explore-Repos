26fe9440b06d9bc0c878b25e2e903ba37dd695db
Dorje <ku.dorjee@gmail.com>
2020-08-10 15:14:57 +0545

    update api call parameter

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 const API = axios.create({ baseURL: 'https://api.github.com/' })
 
-export function searchRepo(search, sort, order){
-  return API.get(`search/repositories?q=${search}&sort=stars&order=desc`)
+export function searchRepo(search, sort, order, page){
+  return API.get(`search/repositories?q=${search}&sort=${sort}&order=${order}&page=${page}`)
 }
 
 export function getRepo(url){
