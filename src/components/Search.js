580e3644ec16025476e124238b990badd3035f79
Dorje <ku.dorjee@gmail.com>
2020-08-10 15:15:25 +0545

    remove file

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 import { InputGroup, FormControl } from 'react-bootstrap'
 import { SearchWrapper } from './styled-component.styled.js';
 import { FiSearch } from 'react-icons/fi';
-import './style.css';
 
 //search input component
 export default function Search(props) {
