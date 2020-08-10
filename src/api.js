68f50cbf83f331d6baf08f17c78e6ce1538af03a
Dorje <ku.dorjee@gmail.com>
2020-08-10 11:19:19 +0545

    add api for repo detail

--- src/api.js
+++ src/api.js
@@ -4,3 +4,7 @@ const API = axios.create({ baseURL: 'https://api.github.com/' })
 export function searchRepo(search, sort, order){
   return API.get(`search/repositories?q=${search}&sort=stars&order=desc`)
 }
+
+export function getRepo(url){
+  return API.get(url)
+}
